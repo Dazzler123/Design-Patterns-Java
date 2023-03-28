75cccc235bbe32acbe32aeb2675b83b2711e1bb4
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-28 21:02:28 +0530

    Content updated

--- Behavioural-Patterns/src/Observer_Pattern/service/Subject.java
+++ Behavioural-Patterns/src/Observer_Pattern/service/Subject.java
@@ -1,4 +1,9 @@
 package Observer_Pattern.service;
 
 public interface Subject {
+    void register(Observer ob);
+
+    void unregister(Observer ob);
+
+    void notifyAllObservers(String message);
 }
