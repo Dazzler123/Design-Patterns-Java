b75ad5d22b640ec4bf5180f2b632550ec8d75b14
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-28 20:59:55 +0530

    Content added

--- Behavioural-Patterns/src/Observer_Pattern/Main.java
+++ Behavioural-Patterns/src/Observer_Pattern/Main.java
@@ -1,4 +1,29 @@
 package Observer_Pattern;
 
 public class Main {
+    /* ========  Observer Design Pattern  ========*/
+    /*
+     * Define an One-to-Many dependency between objects where a state change in one object results
+     * in all its dependents being notified and update automatically.
+     * */
+    public static void main(String[] args) {
+
+        //Initialize the Subject
+        Kamali kamali = new Kamali();
+
+        //Initialize two Observers
+        Dasun dasun = new Dasun();
+        Ramal ramal = new Ramal();
+        Sithum sithum = new Sithum();
+
+        //Register observers to the Subject
+        kamali.register(dasun);
+        kamali.register(ramal);
+        kamali.register(sithum);
+
+        //Now see what will happen after the subject states changed
+        kamali.notifyAllObservers("Kamali Uploaded a New Photo");
+        kamali.notifyAllObservers("Kamali left the chat");
+
+    }
 }
