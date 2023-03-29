08db209008de80aa3bc508665580eb10ffe5f9fc
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 20:11:04 +0530

    Introduced Customer and content added

---
+++ Structural-Patterns/src/Facade_Pattern/Customer.java
@@ -0,0 +1,23 @@
+package Facade_Pattern;
+
+public class Customer implements CRUDFacade{
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void getAll() {
+
+    }
+}
