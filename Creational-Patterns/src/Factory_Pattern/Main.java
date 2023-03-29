37abd34badb58294354f76b7151e70e0ef48f724
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 14:19:18 +0530

    Content updated

--- Creational-Patterns/src/Factory_Pattern/Main.java
+++ Creational-Patterns/src/Factory_Pattern/Main.java
@@ -1,7 +1,16 @@
 package Factory_Pattern;
 
 public class Main {
+
+    //======== Factory Pattern =======
+
+    /* Create object without exposing the creation logic to the client and
+     refer to newly created object using a common interface. */
+
     public static void main(String[] args) {
+        //giving the object keyword we want as an argument of getInstance method in factory and assigning
+        //it to the OS interface reference
+
         OS windows = OSFactory.getInstance("Windows");
         windows.spec();
 
@@ -10,5 +19,8 @@ public class Main {
 
         OS ios = OSFactory.getInstance("IOS");
         ios.spec();
+
+        //in this way the object creation using new keyword is hidden (no new Windows() or new Android() is given)
+        //if we want to add a new OS type in the future, we will have to only update the factory.
     }
 }
