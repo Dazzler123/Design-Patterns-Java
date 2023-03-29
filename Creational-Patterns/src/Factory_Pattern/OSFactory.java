0e3e8202ffe13c42f5e0e8d86a0f580eae5c7199
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 14:05:52 +0530

    Content updated

--- Creational-Patterns/src/Factory_Pattern/OSFactory.java
+++ Creational-Patterns/src/Factory_Pattern/OSFactory.java
@@ -1,4 +1,25 @@
 package Factory_Pattern;
 
 public class OSFactory {
+    public static OS getInstance(String os) {
+//        if (os.equals("Windows")) {
+//            return new Windows();
+//        } else if (os.equals("Android")) {
+//            return new Android();
+//        } else if (os.equals("IOS")) {
+//            return new IOS();
+//        } else {
+//            return new Object();
+//        }
+
+        switch (os) {
+            case "Windows" :
+                return new Windows();
+            case "Android" :
+                return new Android();
+            case "IOS" :
+                return new IOS();
+        }
+        return null;
+    }
 }
