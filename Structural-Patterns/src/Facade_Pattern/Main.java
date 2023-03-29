9985a4cf8dca57e3019770b3f909879d74a4f1af
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 20:17:03 +0530

    Content updated

--- Structural-Patterns/src/Facade_Pattern/Main.java
+++ Structural-Patterns/src/Facade_Pattern/Main.java
@@ -7,6 +7,9 @@ public class Main {
         Hides the complexity of the system by providing
         a common interface to the client.
         (Eg: CrudDAO layer)
+
+        * If there is common repetitive methods in modules, put them into an interface and make it
+         the parent of the relevant modules.
      */
 
     public static void main(String[] args) {
