3be2969666b7941cbc01763c3b234f1d22d0bcca
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 14:00:00 +0530

    Content updated

--- Creational-Patterns/src/Factory_Pattern/Android.java
+++ Creational-Patterns/src/Factory_Pattern/Android.java
@@ -3,6 +3,6 @@ package Factory_Pattern;
 public class Android implements OS{
     @Override
     public void spec() {
-        System.out.println("Operating system in Smartphones.");
+        System.out.println("Operating system in Samsung smartphones.");
     }
 }
