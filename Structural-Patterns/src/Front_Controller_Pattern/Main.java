d475acdd6fb85fa543663b5a12c848cd81c2262d
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 20:51:41 +0530

    Content updated

--- Structural-Patterns/src/Front_Controller_Pattern/Main.java
+++ Structural-Patterns/src/Front_Controller_Pattern/Main.java
@@ -1,6 +1,17 @@
 package Front_Controller_Pattern;
 
 public class Main {
+
+    //======== Front-Controller Design Pattern ===========
+
+    /*
+     A Pattern used to provide a centralized request handling mechanism so all
+     the requests will be handled by a single handler.
+      (Eg: Dispatcher Servlet)
+
+     * Gathers all the requests to a single class and dispatchers it to the relevant places
+     */
+
     public static void main(String[] args) {
 
     }
