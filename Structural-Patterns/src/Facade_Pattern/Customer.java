2d13d59604d1ff260570567ba4aff842682653bf
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 20:12:17 +0530

    Content updated

--- Structural-Patterns/src/Facade_Pattern/Customer.java
+++ Structural-Patterns/src/Facade_Pattern/Customer.java
@@ -3,21 +3,21 @@ package Facade_Pattern;
 public class Customer implements CRUDFacade{
     @Override
     public void save() {
-
+        System.out.println("Customer Saved.");
     }
 
     @Override
     public void update() {
-
+        System.out.println("Customer details updated.");
     }
 
     @Override
     public void delete() {
-
+        System.out.println("Customer deleted.");
     }
 
     @Override
     public void getAll() {
-
+        System.out.println("All Customer details loaded.");
     }
 }
