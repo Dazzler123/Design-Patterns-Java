28bd580dbda3cd17b4adb51e18eb98ce3eddae1e
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 19:31:40 +0530

    Content updated

--- Structural-Patterns/src/Facade_Pattern/Main.java
+++ Structural-Patterns/src/Facade_Pattern/Main.java
@@ -1,4 +1,15 @@
 package Facade_Pattern;
 
 public class Main {
+
+    //======== Facade Design Pattern =========
+    /*
+        Hides the complexity of the system by providing
+        a common interface to the client.
+        (Eg: CrudDAO layer)
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
