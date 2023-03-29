4a87abe71d97619c492c02d62b488a7fc30145f5
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 14:12:53 +0530

    Content updated

--- Creational-Patterns/src/Factory_Pattern/Main.java
+++ Creational-Patterns/src/Factory_Pattern/Main.java
@@ -2,6 +2,13 @@ package Factory_Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        OS windows = OSFactory.getInstance("Windows");
+        windows.spec();
 
+        OS android = OSFactory.getInstance("Android");
+        android.spec();
+
+        OS ios = OSFactory.getInstance("IOS");
+        ios.spec();
     }
 }
--- Creational-Patterns/src/Factory_Pattern/OSFactory.java
+++ Creational-Patterns/src/Factory_Pattern/OSFactory.java
@@ -2,17 +2,8 @@ package Factory_Pattern;
 
 public class OSFactory {
     public static OS getInstance(String os) {
-//        if (os.equals("Windows")) {
-//            return new Windows();
-//        } else if (os.equals("Android")) {
-//            return new Android();
-//        } else if (os.equals("IOS")) {
-//            return new IOS();
-//        } else {
-//            return new Object();
-//        }
-
         switch (os) {
+            //if the asked os type is windows, then return a new object of windows
             case "Windows" :
                 return new Windows();
             case "Android" :
