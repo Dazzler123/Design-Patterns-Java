1b3a775bd384e26e31c91bbe904a8fb30682ba57
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-29 19:27:19 +0530

    Content added

--- Creational-Patterns/src/Object_Pool_Pattern/Main.java
+++ Creational-Patterns/src/Object_Pool_Pattern/Main.java
@@ -1,4 +1,17 @@
 package Object_Pool_Pattern;
 
 public class Main {
+
+    //======= Object Pool Pattern ========
+
+    /*
+    Uses to create a set of initialized objects kept ready for reuse
+    * Creates a limited amount of objects and initializes them.
+    * Reuses the objects whenever they are requested
+    * Destroys them when the application is stopped.
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
